7300720a39a854c0a397b9ebc47fe52f11755965
Maryam (Salvia) Tagharobi <susaanalytics.tgh@gmail.com>
2025-12-15 17:01:54 +1300

    Update sales_analysis_queries.sql

--- 01_customer_sales_insights/sql/sales_analysis_queries.sql
+++ 01_customer_sales_insights/sql/sales_analysis_queries.sql
@@ -2,3 +2,14 @@
 -- Dataset: Online Retail
 -- Author: SusaAnalytics
 -- Purpose: Exploratory sales and customer analysis
+
+##column in dataset:   #InvoiceNo # StockCode  #Description #Quantity   #InvoiceDate   #UnitPrice  #CustomerID  #Country
+
+-- 1. Monthly revenue trend
+SELECT
+    DATE_TRUNC('month', InvoiceDate) AS month,
+    SUM(Quantity * UnitPrice) AS total_revenue
+FROM online_retail
+WHERE Quantity > 0
+GROUP BY DATE_TRUNC('month', InvoiceDate)
+ORDER BY month;
